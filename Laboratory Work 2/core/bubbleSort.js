3534a1f5dc258d0f1386b581feb6c98c4549b86d
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-04-19 18:13:32 +0300

    feat: complete bubble sort

---
+++ Laboratory Work 2/core/bubbleSort.js
@@ -0,0 +1,30 @@
+const bubbleSort = (arr) => {
+    // Copy array by value
+    arr = arr.slice();
+    let steps = 0;
+
+    for (let iteration = 0; iteration < arr.length; iteration++) {
+        for (let cursor = 0; cursor < arr.length - 1; cursor++) {
+            const leftItem = arr[cursor];
+            const rightItem = arr[cursor + 1];
+
+            if (leftItem > rightItem) {
+                arr[cursor + 1] = leftItem;
+                arr[cursor] = rightItem;
+            }
+
+            steps++;
+        }
+    }
+
+    return [arr, steps];
+};
+
+const unsortedArr = ["c", "b", "d", "z", "a"];
+
+const [sortedArr, steps] = bubbleSort(unsortedArr);
+
+console.log("unsortedArr", unsortedArr);
+console.log("sortedArr", sortedArr, `| O(bubble(${unsortedArr.length})) =`, steps);
+
+export default bubbleSort;
