41211c31e5f0740cd7c03f0c136b1690c6130b1d
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-04-21 21:36:08 +0300

    feat: successfully subclass Array

--- Laboratory Work 3/core/main.js
+++ Laboratory Work 3/core/main.js
@@ -1,7 +1,7 @@
 import "../style.css";
-import sortableList from "./sortableList";
-const s = sortableList([3, 2, 1]);
+import SortableList from "./sortableList";
 
-console.log("s", s);
-const sorted = s.bubbleSort();
-console.log("s", sorted, s.sortSteps);
+// const s = new SortableList(3, 2, 1);
+// console.log("s", s);
+// const sorted = s.bubbleSort();
+// console.log("s", sorted, s.sortSteps);
--- Laboratory Work 3/core/sortableList/index.js
+++ Laboratory Work 3/core/sortableList/index.js
@@ -1,23 +1,59 @@
 import defaultCompareFn from "./defaultCompareFn";
 import { bubbleSort } from "../sortingAlgorithms";
 
-const sortableList = (list) => {
-    if (!list) list = [];
-    list.bubbleSort = function (compareFn) {
-        if (!compareFn) compareFn = defaultCompareFn;
-        const [sorted, steps] = bubbleSort(this, compareFn);
-        this.sortSteps = steps;
-        return sorted;
-    };
+// const sortableList = (list) => {
+//     if (!list) list = [];
+//     list.bubbleSort = function (compareFn) {
+//         if (!compareFn) compareFn = defaultCompareFn;
+//         const [sorted, steps] = bubbleSort(this, compareFn);
+//         this.sortSteps = steps;
+//         return sorted;
+//     };
 
-    return list;
-};
+//     return list;
+// };
 
 // Problem:
 // s.bubbleSort()          - works
 // s.slice().bubbleSort()  - doesn't work
 //
-// To avoid this kind of problem, need to use my own type of object constructor.
+// To avoid this kind of problem, definition of custom type with object constructor is needed.
 // https://stackoverflow.com/a/948379
 
-export default sortableList;
+// // Object constructor
+// function SortableList(list) {
+//     this.list = list ? list : [];
+//     this.sortSteps = null;
+//     this.bubbleSort = function (compareFn) {
+//         if (!compareFn) compareFn = defaultCompareFn;
+//         const [sorted, steps] = bubbleSort(this, compareFn);
+//         this.sortSteps = steps;
+//         return sorted;
+//     };
+//     // return this.list;
+// }
+// // Subclassing Array with object constructor
+// // https://stackoverflow.com/a/21709458
+// SortableList.prototype = Object.create(Array.prototype);
+
+// const s = new SortableList([3, 2, 1]);
+// console.log("s", s);
+// console.log("sort?", s.slice().sort());
+// console.log("s", s);
+
+class SortableList extends Array {
+    sortSteps = null;
+    bubbleSort(compareFn) {
+        if (!compareFn) compareFn = defaultCompareFn;
+        const [sorted, steps] = bubbleSort(this, compareFn);
+        this.sortSteps = steps;
+        return sorted;
+    }
+}
+
+const s = new SortableList(3, 2, 1);
+console.log("s", s);
+const ss = s.slice().bubbleSort();
+console.log("s", s, "ss", ss);
+
+export default SortableList;
