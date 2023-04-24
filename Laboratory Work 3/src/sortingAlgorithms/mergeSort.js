72713e72b32af4b4938c0d8b66b11ccc511e34b3
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-04-24 15:24:06 +0300

    feat: add MergeSort

--- Laboratory Work 3/src/sortingAlgorithms/mergeSort.js
+++ Laboratory Work 3/src/sortingAlgorithms/mergeSort.js
@@ -1,10 +1,55 @@
+const halveArray = (array) => {
+    // Integer division
+    const midPoint = Math.trunc(array.length / 2);
+
+    const firstHalf = array.slice(0, midPoint);
+    const secondHalf = array.slice(midPoint);
+
+    return [firstHalf, secondHalf];
+};
+
+const merge = (a, b, compareFn, stepsCounter) => {
+    const arr = [];
+
+    while (a.length && b.length) {
+        const aFirst = a[0];
+        const bFirst = b[0];
+        if (compareFn(aFirst, bFirst) < 0) {
+            // Remove a's first element and add it to the new array
+            a.shift();
+            arr.push(aFirst);
+        } else {
+            // Remove b's first element and add it to the new array
+            b.shift();
+            arr.push(bFirst);
+        }
+        stepsCounter.count++;
+    }
+
+    return [...arr, ...a, ...b];
+};
+
+const recMergeSort = (arr, compareFn, stepsCounter) => {
+    if (arr.length < 2) return arr;
+
+    const [leftHalf, rightHalf] = halveArray(arr);
+    const leftSorted = recMergeSort(leftHalf, compareFn, stepsCounter);
+    stepsCounter.count++;
+    const rightSorted = recMergeSort(rightHalf, compareFn, stepsCounter);
+    stepsCounter.count++;
+
+    return merge(leftSorted, rightSorted, compareFn, stepsCounter);
+};
+
 const mergeSort = (arr, compareFn) => {
-    let steps = 0;
+    let stepsCounter = { count: 0 };
     const t0 = performance.now();
+
+    arr = recMergeSort(arr, compareFn, stepsCounter);
 
     const t1 = performance.now();
     const time = (t1 - t0) / 1000;
-    return [arr, steps, time];
+    return [arr, stepsCounter.count, time];
 };
 
 export default mergeSort;
--- Laboratory Work 3/test/sortableList.test.js
+++ Laboratory Work 3/test/sortableList.test.js
@@ -1,7 +1,7 @@
 import { assert, expect, describe, it } from "vitest";
 import SortableList from "../src/sortableList/index.js";
 
-const customSortTest = (sortingAlgorithmName) => {
+const customSortTest = (sortingAlgorithmName, inplace = true) => {
     const unsortedList = [-10, 20, 1, 2, 0, 2, 2, 3];
     const sortableList = new SortableList(...unsortedList);
     const sortableList2 = SortableList.from(unsortedList);
@@ -13,11 +13,17 @@ const customSortTest = (sortingAlgorithmName) => {
     const sortedSortableCopy = Array.from(sortedSortableList);
     const sortedList = unsortedList.slice().sort();
 
-    expect(sortedSortableReference.length).toBe(sortedList.length);
-    expect(sortedSortableReference).toStrictEqual(sortedSortableCopy);
-    expect(sortedSortableReference).toStrictEqual(sortedList);
-    expect(sortedSortableReference).not.toStrictEqual(unsortedList);
+    it("Sorts in lexicographical order by default", () => {
+        expect(sortedSortableCopy.length).toBe(sortedList.length);
+        expect(sortedSortableCopy).toStrictEqual(sortedList);
+        expect(sortedSortableCopy).not.toStrictEqual(unsortedList);
+    });
 
+    if (inplace) {
+        it("Works inplace", () => {
+            expect(sortedSortableCopy).toStrictEqual(sortedSortableReference);
+        });
+    }
     it("Accepts compareFn as an argument", () => {
         const descendingOrderFn = (a, b) => b - a;
 
@@ -29,7 +35,7 @@ const customSortTest = (sortingAlgorithmName) => {
         expect(sortedSortable).not.toStrictEqual(unsortedList);
     });
 
-    it("Has .sortSteps", () => {
+    it(`Has .sortSteps (steps: ${sortableList.sortSteps})`, () => {
         expect(sortableList.sortSteps).toBeTypeOf("number");
         // console.log(sortingAlgorithmName, "steps:", sortableList.sortSteps);
     });
@@ -77,7 +83,7 @@ describe("SortableList", () => {
             expect(sortedSortableReference).toStrictEqual(sortedList);
             expect(sortedSortableReference).not.toStrictEqual(unsortedList);
 
-            it("Has null .sortSteps", () => {
+            it(`Has null .sortSteps (steps: ${sortableList.sortSteps})`, () => {
                 expect(sortableList.sortSteps).toBeNull();
                 expect(sortableList.slice().sortSteps).toBeNull();
             });
@@ -101,7 +107,7 @@ describe("SortableList", () => {
             expect(sortedSortable).toStrictEqual(sortedList);
             expect(sortedSortable).not.toStrictEqual(unsortedList);
 
-            it("Has .sortSteps", () => {
+            it(`Has .sortSteps (steps: ${sortableList.sortSteps})`, () => {
                 expect(sortableList.sortSteps).toBeTypeOf("number");
             });
             it("Has .sortTime", () => {
@@ -111,7 +117,7 @@ describe("SortableList", () => {
         describe("Implementation of CombSort inplace", () => customSortTest("combSort"));
         describe("Implementation of HeapSort inplace", () => customSortTest("heapSort"));
         describe("Implementation of InsertionSort inplace", () => customSortTest("insertionSort"));
-        // describe("Implementation of MergeSort inplace", () => customSortTest("mergeSort"));
+        describe("Implementation of MergeSort", () => customSortTest("mergeSort", false));
         describe("Implementation of QuickSort inplace", () => customSortTest("quickSort"));
     });
 });
